b5d26ad75ba530e18359fa7761b8faed4a79c157
Al-iii <anh191204@gmail.com>
2025-06-13 10:37:54 +0700

    Update 9filepage.php

--- pages/9filepage.php
+++ pages/9filepage.php
@@ -3,6 +3,54 @@ include '../config/koneksi.php';
 include '../config/session.php';
 
 $logo = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM konten_statis WHERE gambar = 'logo-notezque.svg'"));
+
+$id_user = $_SESSION['id_user'];
+$dir = "../file/";
+
+// Menampilkan daftar file di folder yang dipilih
+$folder_id = $_GET['folder_id'];
+$sql = "SELECT * FROM tambahfile WHERE id_folder = $folder_id";
+$result = $conn->query($sql);
+
+// Menampilkan nama folder
+$sql_folder = "SELECT * FROM tambahfolder WHERE id_folder = $folder_id";
+$folder_result = $conn->query($sql_folder);
+$folder = $folder_result->fetch_assoc();
+
+// Menangani penghapusan file
+if (isset($_GET['delete_id'])) {
+    $delete_id = $_GET['delete_id'];
+    $sql = "DELETE FROM tambahfile WHERE id_file = $delete_id";
+    $conn->query($sql);
+}
+
+// Menangani penambahan file
+if (isset($_POST['simpan'])) {
+    $id_user = $_SESSION['id_user'];
+    $id_folder = $folder['id_folder'];
+    $file = $_FILES['nama_file'];
+    $nama_file = $file['name'];
+    $tmp_nama_file = $file['tmp_name'];
+    $ext = strtolower(pathinfo($nama_file, PATHINFO_EXTENSION));
+
+    $target = $dir . $nama_file;
+    $i = 1;
+    while (file_exists($target)) {
+        $base_name = pathinfo($nama_file, PATHINFO_FILENAME);
+        $target = $dir . $base_name . "_$i." . $ext;
+        $nama_file = $base_name . "_$i." . $ext;
+        $i++;
+    }
+
+    if (move_uploaded_file($tmp_nama_file, $target)) {
+    $sql = "INSERT INTO tambahfile (id_user, id_folder, nama_file) 
+            VALUES ('$id_user', '$id_folder', '$nama_file')";
+    $conn->query($sql);
+  }
+
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +60,12 @@ $logo = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM konten_statis WHER
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NotezQue</title>
     <link rel="icon" type="image/x-icon" href="../uploads/<?= $logo['gambar'] ?>">
-    <link rel="stylesheet" href="../asset/css/9file.css">
-    <link rel="stylesheet" href="../asset/font/Font.css">
-    <link rel="stylesheet" href="../asset/attributes/Atribute1.css">
-    <link rel="stylesheet" href="../asset/attributes/Atribute2.css">
-    <link rel="stylesheet" href="../asset/attributes/Atribute3.css">
+    <link rel="stylesheet" href="../Asset/CSS/9file.css">
+    <link rel="stylesheet" href="../Asset/CSS/87tambahMateri.css">
+    <link rel="stylesheet" href="../Asset/font/Font.css">
+    <link rel="stylesheet" href="../Asset/attributes/Atribute1.css">
+    <link rel="stylesheet" href="../Asset/attributes/Atribute2.css">
+    <link rel="stylesheet" href="../Asset/attributes/Atribute3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -73,109 +122,58 @@ $logo = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM konten_statis WHER
     <main id="main" class="main">
         <div class="main-container">
             <div class="bannerJudul">
-                <img src="https://images.shiksha.com/mediadata/ugcDocuments/images/wordpressImages/2021_12_Difference-Between-Algorithm-and-Program_480x360.jpg" alt="diskrit.jpg">
-                <h1>Algoritma dan Pemograman</h1>
+                <?php if (!empty($folder['gambar'])): ?>
+                    <img class="banner" src="../GambarFolder/<?php echo $folder['gambar']; ?>" alt="Gambar Folder">
+                <?php endif; ?>
+                <h1><?php echo $folder['nama_materi']; ?></h1>
                 <div class="btn">
                     <button class="cd-btn cd-btn--primary" aria-controls="dialog-1">Tambah Folder</button>
                   </div>
             </div>
 
-            <div class="folderTugas">
-                <div class="folder">
-                        <a href="10isimateri.php"><iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon></a>
-                        <a href="10isimateri.php">
-                            <h4 class="namfolder">Percabangan</h4></a>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                    <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                    <h4 class="namfolder" >Nested Loop</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div class="folder">
-                        <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                        <h4 class="namfolder">Modular</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                        <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                        <h4 class="namfolder" >If Else</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                        <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                        <h4 class="namfolder" >Perulangan</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                        <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                        <h4 class="namfolder" >Switch case</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                    <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                    <h4 class="namfolder" >For Loop</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="folder">
-                    <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
-                    <h4 class="namfolder">Do While</h4>
-                    <div class="dropdown">
-                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="hapus()" class="hapus">Hapus</a>
-                            <a href="#" onclick="hapus()" class="edit">Edit</a>
-                        </div>
-                    </div>
-                </div>
-                </div>
+      <div id="dialog-1" class="dialog js-dialog modal-tbhtgs" data-animation="on">
+        <div class="dialog__content modal" role="alertdialog" aria-labelledby="dialog-title-1"
+          aria-describedby="dialog-description-1">
+          <h4 id="dialog-title-1" class="dialog__title">Tambah Tugas</h4>
+          <form action="" method="post" enctype="multipart/form-data">
+            <div id="dialog-description-1 inputMK" class="dialog__description add-task">
+              <input type="file" id="nama_file" name="nama_file" required>
             </div>
+            <footer class="dialog__footer">
+              <button class="cd-btn cd-btn--subtle js-dialog__close batal">Batalkan</button>
+              <button type="submit" class="cd-btn cd-btn--accent buat" name="simpan">Simpan</button>
+            </footer>
+          </form>
+        </div>
+      </div>
+
+            <div class="folderTugas">
+                    <?php
+              if ($result && mysqli_num_rows($result) > 0) {
+              while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="folder">
+                        <a href="../file/' . $row['nama_file'] . '">
+                            <iconify-icon class="i-folder" icon="fxemoji:folder" width="32" height="32"></iconify-icon>
+                        </a>
+                        <a href="../file/' . $row['nama_file'] . '">
+                            <h4 class="namfolder">' . $row['nama_file'] . '</h4>
+                        </a>
+                    <div class="dropdown">
+                    <i  style="color: rgb(0, 0, 0);"><iconify-icon icon="proicons:more" width="28" height="28"></iconify-icon></i>
+                        <div class="dropdown-content">
+                            <a href="?delete_id=' . $row['id_file'] . '" onclick="hapus()" class="hapus">Hapus</a>
+                            <a href="?edit_id=' . $row['id_file'] . '" onclick="hapus()" class="edit">Edit</a>
+                        </div>
+                    </div>
+                </div>';
+              }
+            }
+            ?>
+            </div>
+        </div>
     </main>
 
-    <div class="modal-tbhtgs">
+    <!-- <div class="modal-tbhtgs">
         <div class="modal">
             <form action="/submit">
                 <div class="inputan">
@@ -187,11 +185,13 @@ $logo = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM konten_statis WHER
                 </div>
             </form>
         </div>
-    </div>
+    </div> -->
+    
     <?php include 'footer.php' ?>
     <script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>
-    <script src="../asset/attributes/Atribute1.js"></script>
-    <script src="../asset/attributes/Atribute2.js"></script>
-    <script src="../asset/JS/9file.js"></script>
+    <script src="../Asset/attributes/Atribute1.js"></script>
+    <script src="../Asset/attributes/Atribute2.js"></script>
+    <!-- <script src="../Asset/JS/9file.js"></script> -->
+    <script src="../Asset/JS/87tambahMateri.js"></script>
 </body>
 </html>
